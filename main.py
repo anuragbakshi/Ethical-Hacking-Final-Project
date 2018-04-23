5cfdd2a4fe76ff1372fcff195aa4f1d21bdc1733
Cameron Reynoldson <cameronreynoldson@utexas.edu>
2018-04-23 16:38:57 -0500

    login button

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import logging
 
 from flask import abort, render_template, request, jsonify
 from sqlalchemy import column, literal, literal_column, text
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 import models
+import json
 
 from common import app, db
 
@@ -11,6 +14,23 @@ from common import app, db
 def dashboard():
 	creds = models.FacebookCredential.query.all()
 	return render_template('dashboard.html', credentials=creds)
+
+@app.route('/login', methods=['POST'])
+def login():
+	data = json.loads(request.data)
+	username = data["username"]
+	password = data["password"]
+	driver = webdriver.Firefox()
+	# or you can use Chrome(executable_path="/usr/bin/chromedriver")
+	driver.get("http://www.facebook.org")
+	assert "Facebook" in driver.title
+	elem = driver.find_element_by_id("email")
+	elem.send_keys(username)
+	elem = driver.find_element_by_id("pass")
+	elem.send_keys(password)
+	elem.send_keys(Keys.RETURN)
+	# driver.close()
+	return ('', 204)
 
 if __name__ == '__main__':
     from os import sys, path
